3a73884ca0c281e1c797fc6ed8c5c278ae6c1179
Jose Honorato <jlhonora@ing.puc.cl>
2014-08-25 06:21:10 -0700

    Added sleep for more interesting interaction

--- code/c-arduino/hello_world/hello.c
+++ code/c-arduino/hello_world/hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char const* argv[]) {
-    printf("Hello, World!\n");
+    while (1) {
+        printf("Hello, World!\r\n");
+        sleep(1);
+    }
     return 0;
 }
