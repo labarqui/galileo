7f4d3c0b0480b989d4cff85f752d7edf08240a5e
jlhonora <jlhonora@ing.puc.cl>
2014-09-14 23:40:58 -0300

    Added example for i2c lcd

---
+++ code/c-arduino/lcd/lcd_i2c/lcd_i2c.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+#define I2C_ADDR 0x27
+#define BACKLIGHT_PIN 3
+#define EN_PIN 2
+#define RW_PIN 1
+#define RS_PIN 0
+#define D4_PIN 4
+#define D5_PIN 5
+#define D6_PIN 6
+#define D7_PIN 7
+
+LiquidCrystal_I2C lcd(I2C_ADDR, EN_PIN, RW_PIN, RS_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
+  lcd.setBacklight(HIGH);  
+  lcd.home();
+  lcd.print("Arqui LCD");  
+}
+
+int n = 1;
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(n++, DEC);
+  lcd.setBacklight(LOW);
+  delay(1000);
+  lcd.setBacklight(HIGH);
+  delay(1000);
+}
